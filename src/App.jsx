f8c734e69dce26d1a5a2e141329085b44d5ca12f
yaminmusleh <yaminaktham3@gmail.com>
2026-02-07 00:53:19 +0200

    added portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AppNavbar from "./assets/navbar/AppNavbar";
 import Hero from "./assets/hero/Hero";
+import Portfolio from "./assets/portfolio/Portfolio";
 
 export default function App() {
   return (
     <>
       <AppNavbar />
       <Hero />
+      <Portfolio />
     </>
   );
 }
--- src/assets/portfolio/Portfolio.jsx
+++ src/assets/portfolio/Portfolio.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./portfolio.module.css";
+import image from "./../imgs/cabin.png";
+import image1 from "./../imgs/cake.png";
+import image2 from "./../imgs/circus.png";
+import image3 from "./../imgs/game.png";
+import image4 from "./../imgs/safe.png";
+import image5 from "./../imgs/submarine.png";
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
-  )
+    <div
+      className={` ${styles.portfoliosection} justify-content-center align-items-center d-flex flex-column gap-5`}
+    >
+      <div>
+        <h2  className={styles.title}>Portfolio</h2>
+      </div>
+      <div className="container d-flex flex-column gap-3">
+        <div className="row g-4">
+          <div className="col-md-4">
+            <div className="card rounded-3 overflow-hidden">
+              <img src={image} alt="img1" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card rounded-3 overflow-hidden">
+              <img src={image1} alt="img2" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card rounded-3 overflow-hidden">
+              <img src={image2} alt="" />
+            </div>
+          </div>
+        </div>
+
+        {/* Second row of 3 cards */}
+        <div className="row g-4">
+          <div className="col-md-4">
+            <div className="card rounded-3 overflow-hidden"><img src={image3} alt="" /></div>
+          </div>
+          <div className="col-md-4">
+            <div className="card rounded-3 overflow-hidden"><img src={image4} alt="" /></div>
+          </div>
+          <div className="col-md-4">
+            <div className="card rounded-3 overflow-hidden"><img src={image5} alt="" /></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
